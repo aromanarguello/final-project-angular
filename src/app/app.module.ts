46d422154f47d06888ba3189e04dcdd12b5c5142
Alejandro Roman <alejandroroman@Alejandros-MacBook-Pro-3.local>
2017-12-09 23:58:06 -0500

    created components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './home-page/about-page/about-page.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RecipesPageComponent } from './recipes-page/recipes-page.component';
+import { SurveyComponent } from './profile/survey/survey.component';
+import { ResultsPageComponent } from './profile/results-page/results-page.component';
+import { NotComponent } from './not/not.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ProfileComponent,
+    RecipesPageComponent,
+    SurveyComponent,
+    ResultsPageComponent,
+    NotComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

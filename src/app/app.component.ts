e0ea56aefc82e022ab11298a3bf0b0f556d6b612
Alejandro Roman <alejandroroman@Alejandros-MacBook-Pro-3.local>
2017-12-11 15:10:34 -0500

    finished login page and connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserApiService } from './services/user-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor (private userThing: UserApiService) {}
+
 }
--- src/app/services/recipe-api.service.ts
+++ src/app/services/recipe-api.service.ts
@@ -5,4 +5,6 @@ export class RecipeApiService {
 
   constructor() { }
 
+
+
 }

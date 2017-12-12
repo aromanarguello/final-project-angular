25da80dd357f212323955d8b736dcac91fc3443d
Alejandro Roman <alejandroroman@Alejandros-MacBook-Pro-3.local>
2017-12-11 23:21:02 -0500

    connected survey form to backend

--- src/app/profile/survey/survey.component.ts
+++ src/app/profile/survey/survey.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecipeApiService, Survey } from '../../services/recipe-api.service';
 
 @Component({
   selector: 'app-survey',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SurveyComponent implements OnInit {
 
-  constructor() { }
+ theSurvey = new Survey();
+
+  constructor(
+    private routerThing: Router,
+    private recipeThing: RecipeApiService
+  ) { }
 
   ngOnInit() {
+  }
+
+  surveyAjax(){
+    this.recipeThing.postSurvey(this.theSurvey)
+    .then( () => {
+      this.routerThing.navigate(['/profile'])
+    })
+    .catch( err => {
+      alert("Sorry something went wrong");
+      console.log("Signup Error")
+      console.log(err)
+    })
   }
 
 }
--- src/app/services/recipe-api.service.ts
+++ src/app/services/recipe-api.service.ts
@@ -1,10 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+
+import { environment } from '../../environments/environment';
+
+export class Survey {
+  height:      number;
+  weight:      number;
+  age:         number;
+  peanut:      boolean;
+  lactose:     boolean;
+  energyOne:   number;
+  energyTwo:   number;
+  energyThree: number;
+}
 
 @Injectable()
 export class RecipeApiService {
 
-  constructor() { }
+  currentSurvey: Survey;
 
+  constructor(private httpThing: HttpClient) { }
+
+  postSurvey(surveyInfo: Survey){
+    return this.httpThing.post(
+      `${environment.backendUrl}/api/survey`,
+      surveyInfo
+    )
+    .toPromise()
+    .then((apiResults: any) => {
+      this.currentSurvey = apiResults.surveyInfo;
+      return apiResults;
+    })
+  }
 
 
 }
